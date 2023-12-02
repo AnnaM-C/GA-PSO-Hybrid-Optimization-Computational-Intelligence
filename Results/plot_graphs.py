12e2ddb3f046e88c4e90cb6455efbf89312175b1
AnnaM-C <carter.anna.maria@gmail.com>
2023-12-02 15:10:15 +0000

    Add new SGD and NSGA2 training log and

--- Results/plot_graphs.py
+++ Results/plot_graphs.py
@@ -12,14 +12,19 @@ def plot_NSGA_vs_SGD(NSGA_file, SGD_file):
     df_SGD=pd.read_csv(SGD_file)
     SGD_max_accuracy_column=df_SGD["accuracy"]
 
-    plt.plot(range(100), SGD_max_accuracy_column)
-    plt.plot(range(100), NSGA_max_accuracy_column)
+    plt.plot(range(99), SGD_max_accuracy_column, label='SGD')
+    plt.plot(range(100), NSGA_max_accuracy_column, label="NSGA2")
 
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.title('Comparison of NSGA vs SGD Max Accuracy over Epochs')
+
+    plt.legend()
     plt.show()
 
 
 
 if __name__ == "__main__":
-    NSGA_file= "training_log_3.csv"
+    NSGA_file= "training_log_4.csv"
     SGD_file="SDG_training_log.csv"
     plot_NSGA_vs_SGD(NSGA_file, SGD_file)
