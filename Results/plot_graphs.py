ea95a019cd73386a2088dce369c29d1988a74f9e
AnnaM-C <carter.anna.maria@gmail.com>
2023-12-07 01:55:32 +0000

    Add SL-PSO

--- Results/plot_graphs.py
+++ Results/plot_graphs.py
@@ -4,27 +4,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_NSGA_vs_SGD(NSGA_file, SGD_file):
+def plot_NSGA_vs_SGD(NSGA_file, SSPSO_file):
 
     df_NSGA=pd.read_csv(NSGA_file)
+    NSGA_avg_accuracy_column=df_NSGA["avg"]
     NSGA_max_accuracy_column=df_NSGA["max"]
 
-    df_SGD=pd.read_csv(SGD_file)
-    SGD_max_accuracy_column=df_SGD["accuracy"]
+    df_SSPSO=pd.read_csv(SSPSO_file)
 
-    plt.plot(range(99), SGD_max_accuracy_column, label='SGD')
-    plt.plot(range(100), NSGA_max_accuracy_column, label="NSGA-II")
 
-    plt.xlabel('Epochs')
-    plt.ylabel('Accuracy')
-    plt.title('Comparison of NSGA-II vs SGD Accuracy over Epochs')
+
+    plt.plot(range(100), NSGA_max_accuracy_column, label="NSGA-II Maximum", color="blue")
+    # plt.plot(range(100), NSGA_avg_accuracy_column, label="NSGA-II Average", color="orange")
+    plt.plot(range(100), transposed, color="pink")
+
+    plt.xlabel('Generations')
+    plt.ylabel('Maximum Accuracy')
+    plt.title('Comparison of NSGA-II vs Genetic Algorithm Accuracy over Generations')
 
     plt.legend()
     plt.show()
 
-
-
 if __name__ == "__main__":
-    NSGA_file= "training_log_4.csv"
-    SGD_file="SDG_training_log.csv"
-    plot_NSGA_vs_SGD(NSGA_file, SGD_file)
+    NSGA_file= "NSGA/training_log_6.csv"
+    SSPSO="SSPSO/accuracyTrack-070.csv"
+    plot_NSGA_vs_SGD(NSGA_file, SSPSO)
