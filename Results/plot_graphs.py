aaa99c8883922172437e63ecaf1f6248b80b9110
AnnaM-C <carter.anna.maria@gmail.com>
2023-12-08 04:11:30 +0000

    Added SLPSO vs GA plot

--- Results/plot_graphs.py
+++ Results/plot_graphs.py
@@ -60,13 +60,36 @@ def plot_NSGA_vs_SSPSO(NSGA_file, SSPSO_file):
     plt.savefig("nsga-vs-SSPSO.png", dpi=300, bbox_inches='tight')
     plt.show()
 
+def plot_GA_vs_SLPSO(GA_file, SLPSO_file):
+
+    df_GA=pd.read_csv(GA_file)
+    GA_max_accuracy_column=df_GA["Max Fitness"]
+
+    with open(SLPSO_file, 'r') as read_obj: # read csv file as a list of lists
+        csv_reader = csv.reader(read_obj) # pass the file object to reader() to get the reader object
+        SLPSO = list(csv_reader) # Pass reader object to list() to get a list of lists
+        SLPSOVals = [eval(i) for i in SLPSO[4]]
+
+    plt.plot(range(100), GA_max_accuracy_column, label="GA", color="tab:blue")
+    plt.plot(range(100), SLPSOVals, label="SLPSO", color="tab:orange")
+    plt.xlabel('Generations')
+    plt.ylabel('Maximum Accuracy')
+    plt.title('GA vs SLPSO')
+
+    plt.legend()
+    plt.savefig("ga-vs-SLPSO.pdf", bbox_inches='tight')
+    plt.savefig("ga-vs-SLPSO.png", dpi=300, bbox_inches='tight')
+    plt.show()
+
 if __name__ == "__main__":
     NSGA_file   = "/Users/annacarter/Library/Mobile Documents/com~apple~CloudDocs/Documents/Semester One/COMPUTATIONAL INTELLIGENCE COM3013/CW/computational-intelligence/Final Models/NSGA2/training_log_NSGA_1bounds2.csv"
     # NSGA_file   = "/Users/annacarter/Library/Mobile Documents/com~apple~CloudDocs/Documents/Semester One/COMPUTATIONAL INTELLIGENCE COM3013/CW/computational-intelligence/Results/NSGA/training_log_NSGA_1bounds.csv"
     SGD         = "SSPSO/accuracyTrack-070.csv"
     GA_file     = "GA/evolution_stats.csv"
     SSPSO_file  = "/Users/annacarter/Library/Mobile Documents/com~apple~CloudDocs/Documents/Semester One/COMPUTATIONAL INTELLIGENCE COM3013/CW/computational-intelligence/Final Models/SSPSO/0-SSPSO/bestLogbookNE.csv"
+    SLPSO_file  = "/Users/annacarter/Library/Mobile Documents/com~apple~CloudDocs/Documents/Semester One/COMPUTATIONAL INTELLIGENCE COM3013/CW/computational-intelligence/Final Models/SLPSO/0-SLPSO/bestLogbookNE.csv"
     # plot_NSGA_vs_SGD(NSGA_file, NSGA_file)
     # plot_NSGA_vs_GA(NSGA_file, GA_file)
-    plot_NSGA_vs_SSPSO(NSGA_file, SSPSO_file)
+    # plot_NSGA_vs_SSPSO(NSGA_file, SSPSO_file)
+    plot_GA_vs_SLPSO(GA_file, SLPSO_file)
 
